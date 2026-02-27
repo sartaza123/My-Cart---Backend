e9078fd21d6bbd350e593b86c377bad7fd1f8abf
sartaza123 <sartaza123@gmail.com>
2026-02-27 12:07:28 +0530

    mongoDb Atlas is connected using mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://sartaza123_db_user:KKn1zmWVNzId1th5@cluster0.xwsqjv7.mongodb.net/",
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 module.exports = app;
